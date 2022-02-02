883179940cf1fd456ab0826bd86a5efa7a3e2570
Pedro <pedro.trabalhos@outlook.com>
2022-02-02 20:23:24 -0300

    initial base vueJS

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="px-3">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+        <a class="navbar-brand" href="#">Walleet</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Alterna navegação">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link" href="#">Aquisições</a>
+            <a class="nav-item nav-link" href="#">Vendas</a>
+            <a class="nav-item nav-link" href="#">Carteira</a>
+          </div>
+        </div>
+      </nav>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
